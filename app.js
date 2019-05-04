6a20e5a2735255eab15f1325b4873ef0d42d5c6a
Saurav Pratihar <pratihar.saurav@gmail.com>
2019-05-04 19:23:10 +0530

    Docker implemented

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const router = require('./routes')
 const config = require('./config')
 const FUNCTIONS = require('./functions');
 require("dotenv").config();
-const fileUpload = require('express-fileupload');
 
 const app = express()
 const publicRoute = express.Router()
 const protectedRoute = express.Router();
-app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
