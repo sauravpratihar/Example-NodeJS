f113d7afeb99b85359c9eeb1ac81f4d69a0758da
Saurav Pratihar <pratihar.saurav@gmail.com>
2019-05-04 19:52:50 +0530

    Update imageRoute.js

--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,5 +1,5 @@
 const imageController = require('../controllers/imageController')
 
 module.exports = (publicRoute, protectedRoute) => {
-    publicRoute.get('/thumbnail', imageController.createThumbnail)
+    publicRoute.get('/thumbnail', FUNCTIONS.checkParamsGET(['url']), imageController.createThumbnail)
 }
